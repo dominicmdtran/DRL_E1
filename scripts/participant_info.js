a7f621049093cc2ed7a3aaa765f12dc0ee9e3e8c
dominicmdtran <dom.minh.nic.tran@gmail.com>
2025-09-29 11:21:38 +1000

    Add files via upload

--- scripts/participant_info.js
+++ scripts/participant_info.js
@@ -4,80 +4,74 @@ var participant_info = {
     pages: function () {
       return [`
 <div style="padding-left: 40px; padding-right: 40px; text-align: left;">
-  <h1>Participant Information Sheet</h1>
-  <h2 style="color: #e1310e;"> ETH23-8006 - G-16-2025 - Impulsivity and Daily Habits</h2>
+  <h1>Participant Information Statement</h1>
+  <h2 style="color: #e1310e;">Research study: Executive function and response control</h2>
 
-  <h4>WHO IS CONDUCTING THIS RESEARCH?</h4>
-  Our names are Shao-hsuan Huang and Ruhi Kumar and we are students at UTS. Our supervisor is Dominic Tran.
+  <p><strong>Professor Evan Livesey (Responsible Researcher)</strong><br>
+  School of Psychology, Faculty of Science<br>
+  Phone: +61 2 9351 2845 | Email: <a href="mailto:evan.livesey@sydney.edu.au">evan.livesey@sydney.edu.au</a></p>
 
-  <h4>WHAT IS THE RESEARCH ABOUT?</h4>
-  This online research aims to investigate the relationship between impulsivity and daily social media habits among university students and assess whether a correlation is present between a new task developed for testing response inhibition and a pre-existing response inhibition task.
-
-  <h4>WHY HAVE I BEEN INVITED?</h4>
-  You have been invited to participate because you are part of the target population for this study. Specifically, you meet the eligibility criteria, such as being within the appropriate age range and currently enrolled in a psychology subject at UTS.
-
-  <strong>Inclusion criteria:</strong>
+  <h4>What is this study about?</h4>
+  <p>You are invited to take part in a research study investigating how cognitive and individual differences factors influence our ability to act or withhold actions when needed.</p>
+  <p>You have been invited to participate in this study because you have expressed interest through the first- and second-year psychology participant pool (SONA). This Participant Information Statement tells you about the research study. Knowing what is involved will help you decide if you want to take part. Please read carefully and ask questions if anything is unclear.</p>
+  <p><strong>Participation is voluntary.</strong></p>
+  <p>By giving consent you are telling us that you:</p>
   <ul>
-    <li>Students enrolled in psychology courses at the University of Technology Sydney</li>
-    <li>17 years or older</li>
-    <li>Fluent in English (to understand task instructions and questionnaire items)</li>
+    <li>Understand what you have read.</li>
+    <li>Agree to take part in the research study as outlined below.</li>
+    <li>Agree to the use of your personal information as described.</li>
   </ul>
+  <p>You will be emailed a copy of this Participant Information Statement to keep.</p>
 
-  <h4>WHAT DOES MY PARTICIPATION INVOLVE?</h4>
-  Participation in this study is voluntary. It is completely up to you whether or not you decide to take part. If you decide to participate, we will invite you to:
+  <h4>Who is running this study?</h4>
+  <p>The study is being carried out by: Prof. Evan Livesey, Dr. Dominic Tran, Dr. Yvonne Chan, Dr. Justine Greenaway, Illeana Prieto, Athena Cheng, Brendon Popovski, Jack Collins, and Andrew Huang.</p>
+
+  <h4>Who can take part in the study?</h4>
+  <p>The study is open to all who may be interested.</p>
+
+  <h4>What will the study involve for me?</h4>
+  <p>During the experiment, you will be asked to view and respond to visual stimuli on a computer screen. Depending on the version of the experiment, the stimuli may be shapes, objects, faces, words, letters, or other symbols. You may be asked to:</p>
   <ul>
-    <li>Complete an online experiment (including behavioural tasks and self-report measures) which takes around 60 minutes.</li>
-    <li>Read all the instructions carefully and answer all the questions as honestly and accurately as possible.</li>
-    <li>Try not to let your performance/answers on one behavioural task/statement influence your responses to others.</li>
+    <li>Identify a stimulus,</li>
+    <li>Judge its orientation, location, or motion direction,</li>
+    <li>Respond to its appearance using button presses,</li>
+    <li>Use memory and problem-solving abilities,</li>
+    <li>Answer a questionnaire about daily habits.</li>
   </ul>
-  Participation is expected to take 60 minutes and you will receive <strong>2 SONA credits</strong>.
+  <p>The study should take approximately 60 minutes. If you complete it seriously and finish earlier, you will still receive full credit. Please do not share details of the study with others, to avoid biasing their responses.</p>
 
-  <h4>ARE THERE ANY RISKS/INCONVENIENCE?</h4>
-  Although we do not anticipate any risks or discomfort from the behavioural tasks, the social media addiction questionnaire may prompt you to reflect on your social media use and recognise potential negative impacts. 
+  <h4>Can I withdraw once I have started?</h4>
+  <p>Participation is voluntary. Your decision will not affect your relationship with the researchers or the University of Sydney. You may withdraw at any time before the experiment ends by informing the researcher. Starting and submitting your final response indicates your consent.</p>
+  <p>If you withdraw before finishing, no data will be recorded. If you withdraw during the task, you may request that previously collected data be removed. After the final question is submitted, data cannot be withdrawn as it is stored anonymously.</p>
 
-  Please be aware that you have the right to refuse to answer any questions, and you are free to discontinue participation at any time without giving any reason and without any consequences.
+  <h4>Are there any risks or costs?</h4>
+  <p>Aside from giving up your time, we do not expect any risks or costs from participating.</p>
 
-  If you experience distress, please contact your GP or UTS Counselling (9514 1177).
+  <h4>Are there any benefits?</h4>
+  <p>We cannot guarantee direct benefits, but you will help us understand factors that lead to errors and how they can be minimised. You may also learn more about psychological research. You will receive 1 course credit for your participation.</p>
 
-  <strong>Emergency 24/7 support lines:</strong>
-  <ul>
-    <li>Lifeline: 13 11 14</li>
-    <li>NSW Mental Health Line: 1800 011 511</li>
-    <li>Emergency: Call 000</li>
-  </ul>
-  Outside Australia: contact your local mental health crisis service or visit <a href="https://www.befrienders.org" target="_blank">www.befrienders.org</a>.
+  <h4>What will happen to information that is collected?</h4>
+  <p>By consenting, you agree that your experimental responses will be collected for research. Data will only be used for purposes outlined here, unless you provide further consent.</p>
+  <p>Your information will be stored securely and de-identified. Your personal sign-up details will remain confidential. Results may be published in aggregate form but you will not be identifiable.</p>
+  <p>We may use anonymised data for future projects, subject to ethical approval.</p>
 
-  <h4>DO I HAVE TO TAKE PART IN THIS RESEARCH PROJECT?</h4>
-  Participation is voluntary. If you choose not to participate or to withdraw later, it will not affect your relationship with the researchers or UTS. However, you will not receive SONA credit unless the study is completed.
+  <h4>Will I be told the results?</h4>
+  <p>You may request feedback on the overall results. Contact the researcher who conducted your experiment or posted the advertisement. Feedback will be provided after the study in the form of a brief written summary and a captioned figure with aggregate data.</p>
 
-  <h4>WHAT IF I WITHDRAW FROM THIS RESEARCH PROJECT?</h4>
-  You can withdraw at any time by closing your browser. If you do this before completing the study, your data will not be used. 
-  To still receive SONA credit, email Shao-hsuan (Candice) Huang: <a href="mailto:shao-hsuan.huang@student.uts.edu.au">shao-hsuan.huang@student.uts.edu.au</a>.  
-  After submitting the study, data cannot be removed as it is non-identifiable.
+  <h4>What if I would like more information?</h4>
+  <p>You can contact the relevant researcher for more information. Researchers include: Prof. Evan Livesey, Dr. Dominic Tran, Dr. Yvonne Chan, Dr. Justine Greenaway, Illeana Prieto, Athena Cheng, Brendon Popovski, Jack Collins, and Andrew Huang.</p>
 
-  <h4>WHAT WILL HAPPEN TO INFORMATION ABOUT ME?</h4>
-  By consenting, you allow the research team to collect and use your de-identified data. No identifying info like name, email or student ID will be collected. 
+  <h4>What if I have a complaint or concerns?</h4>
+  <p>This study has been approved by the University of Sydney Human Research Ethics Committee [ethics reference: 2020/HE000208] in line with the National Statement on Ethical Conduct in Human Research.</p>
+  <p>If you have concerns or complaints, contact:</p>
+  <p>
+    Human Ethics Manager<br>
+    <a href="mailto:human.ethics@sydney.edu.au">human.ethics@sydney.edu.au</a><br>
+    +61 2 8627 8176
+  </p>
 
-  All data will be stored securely in UTS-compliant systems and deleted from Qualtrics servers after transfer. IP addresses will not be collected. 
-
-  Results may be published or shared through open scientific databases but only in aggregate form that cannot identify individuals.
-
-  <h4>WHAT IF I HAVE ANY QUERIES OR CONCERNS?</h4>
-  You can contact us at:
-  <ul>
-    <li>Shao-hsuan Huang: <a href="mailto:shao-hsuan.huang@student.uts.edu.au">shao-hsuan.huang@student.uts.edu.au</a></li>
-    <li>Ruhi Kumar: <a href="mailto:ruhi.kumar@student.uts.edu.au">ruhi.kumar@student.uts.edu.au</a></li>
-  </ul>
-  You will be given or can download a copy of this form for your records.
-
-  <h4>NOTE ON ETHICS APPROVAL</h4>
-  This study has been approved by the UTS Human Research Ethics Committee. For independent concerns or complaints:
-  <br><br>
-  Ethics Secretariat<br>
-  Phone: +61 2 9514 2478<br>
-  Email: <a href="mailto:Research.Ethics@uts.edu.au">Research.Ethics@uts.edu.au</a><br><br>
-
-  <h4 style="text-align: center; color: #e1310e;">This information sheet is for you to keep.</h4>
+  <h4 style="text-align:center; color:#e1310e;">This information sheet is for you to keep.<br>
+  Ethics ref.: 2020/HE000208 | Version 6, 17/09/2025</h4>
 </div>
       `];
     },
